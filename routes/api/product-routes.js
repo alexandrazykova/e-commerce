27fea912c629d386325ac7a0168c442a9789e4e1
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-18 16:54:43 -0400

    added get by id route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,16 +11,30 @@ router.get('/', async (req, res) => {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
     });
-    res.status(200).json(productDataData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: Tag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'Nothing found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err)
+  }
 
 });
 
