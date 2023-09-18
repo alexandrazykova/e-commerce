2e0203c012db49e57ca7eee31368d3df8cfe0d08
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-17 20:41:57 -0400

    fixed bug in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,7 +28,6 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'Nothing found with that id!' });
       return;
     }
-
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -68,7 +67,6 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'Nothing found with that id!' });
       return;
     }
-
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
