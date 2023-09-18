30a30f5458c3ccd6b7b0c2d21bf0018f05943ae7
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-18 17:30:22 -0400

    added delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag }],
     });
 
     if (!productData) {
@@ -115,8 +115,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'Nothing found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
